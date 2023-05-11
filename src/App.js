9a7480f5e7d799591372238e7e7e1bf07cd3966e
Robert Palmer <61207668+Palm717@users.noreply.github.com>
2023-05-11 11:29:36 -0400

    sideBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./index.css";
+import theme from "./theme"; // Import your custom theme
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route exact path="/" Component={Home} />
+            <Route path="/shop" Component={Shop} />
+          </Routes>
+        </div>
+      </Router>
+    </ChakraProvider>
   );
 }
 
